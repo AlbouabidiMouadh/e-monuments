bd12d5585e8ad7ac0cf80a036d839973454c5847
AlbouabidiMouadh <95055135+AlbouabidiMouadh@users.noreply.github.com>
2023-06-06 17:06:50 +0100

    map finis

--- src/components/UserLocation/UserLocation.js
+++ src/components/UserLocation/UserLocation.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect, useRef} from 'react';
-import MapView, {Callout, Marker, Polyline} from 'react-native-maps';
+import MapView, {Callout, Marker} from 'react-native-maps';
 import {StyleSheet, Text, View} from 'react-native';
 import RoundWhiteButton from './FinLocation';
 import {
@@ -11,7 +11,6 @@ import Geolocation from '@react-native-community/geolocation';
 import axios from 'axios';
 
 export default function UserMap() {
-  const [initialRegion, setInitialRegion] = useState(null);
   const [guides, setGuides] = useState([]);
   const fetchGuides = async () => {
     const response = await axios.get(`http://${url}/guides-coord`);
@@ -28,49 +27,19 @@ export default function UserMap() {
   const mapView = useRef(null);
 
   const [userLocation, setUserLocation] = useState();
-  const [pin, setPin] = useState({
-    latitude: 35.75928,
-    longitude: 10.812208,
-  });
-  const [pin1, setPin1] = useState({
-    latitude: 35.66,
-    longitude: 10.812208,
-  });
-  const [origin, setOrigin] = useState({
-    latitude: 35.75928,
-    longitude: 10.812208,
-    longitudeDelta: 0.04,
-    latitudeDelta: 0.09,
-  });
 
   useEffect(() => {
-    // Geolocation.getCurrentPosition(
-    //   position => {
-    //     const { latitude, longitude } = position.coords;
-    //     setInitialRegion({
-    //       latitude,
-    //       longitude,
-    //       latitudeDelta: 0.0922,
-    //       longitudeDelta: 0.0421,
-    //     });
-    //   },
-    //   error => {
-    //     console.error(error);
-    //   }
-    // );
     fetchGuides();
   }, []);
 
-  /*   useEffect(() => {
-    setTimeout(() => {
-      animateToRegion(mapView, userLocation);
-    }, 1000);
-  }, [userLocation]); */
+  // map
+  // profile posts
+  // traduction francais
+  // traduction final
 
   return (
-    <View>
-      {/* 
-      <RoundWhiteButton
+    <View style={{flex: 1}}>
+      {/* <RoundWhiteButton
         onPress={() => moveToUserLocation(mapView, userLocation)}
         image={require('../../assets/mapCenter.png')}
         disabled={false}
@@ -78,68 +47,41 @@ export default function UserMap() {
       <MapView
         ref={mapView}
         style={styles.map}
-        // initialRegion={{
-        //   latitude: 36.81785,
-        //   longitude: 10.18445,
-        //   longitudeDelta: 0.04,
-        //   latitudeDelta: 0.09,
-        // }}
         initialRegion={{
           latitude: 33.8869,
           longitude: 9.5375,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitudeDelta: 4.5,
+          longitudeDelta: 4.5,
         }}
         provider="google"
-        minZoomLevel={10}
-        maxZoomLevel={20}
         showsUserLocation={userLocation ? true : false}>
-        {/* {guides.map(item => {
+        {guides.map(item => {
           return (
             <Marker
+              key={item._id}
               coordinate={{
                 latitude: parseFloat(item.latitude),
                 longitude: parseFloat(item.longitude),
-              }}
-              // draggable={true}
-              // onDragStart={(e) => {
-              //   console.log(e.nativeEvent.coordinate);
-              // }}
-              // onDragEnd={(e) => {
-              //   setPin({
-              //     latitude: e.nativeEvent.coordinate.latitude,
-              //     longitude: e.nativeEvent.coordinate.longitude,
-              //   });
-              // }}
-            >
-              <Callout>
-                <Text>{item.title}</Text>
+              }}>
+              <Callout tooltip>
+                <View style={styles.calloutContainer}>
+                  <Text style={styles.calloutText}>{item.title}</Text>
+                </View>
               </Callout>
             </Marker>
           );
-        })} */}
+        })}
         {/* <Marker
-          coordinate={pin1}
-          draggable={true}
-          onDragStart={(e) => {
-            console.log(e.nativeEvent.coordinate);
-          }}
-          onDragEnd={(e) => {
-            setPin1({
-              latitude: e.nativeEvent.coordinate.latitude,
-              longitude: e.nativeEvent.coordinate.longitude,
-            });
-          }}
-        >
+          coordinate={{
+            latitude: 33.8869,
+            longitude: 9.5375,
+          }}>
           <Callout>
-            <Text>I'm here</Text>
+            <View>
+              <Text>hello</Text>
+            </View>
           </Callout>
-        </Marker>
-        <Polyline
-          coordinates={[pin1, pin]}
-          strokeColor="red"
-          strokeWidth={6}
-        /> */}
+        </Marker> */}
       </MapView>
     </View>
   );
@@ -147,9 +89,19 @@ export default function UserMap() {
 
 const styles = StyleSheet.create({
   map: {
+    flex: 1,
     width: '100%',
     height: '100%',
     zIndex: -1,
-    // marginTop: 50,
+  },
+  calloutContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 10,
+    padding: 10,
+  },
+  calloutText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: "black"
   },
 });
